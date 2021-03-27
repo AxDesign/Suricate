e164564c515f69e04ffda6f75f66c7ebf2d45ac6
AxDesign <contact.alexrobert04@gmail.com>
2021-03-27 17:49:44 +0100

    FIX - Envoie en base de donnée

--- server/app/sendData.php
+++ server/app/sendData.php
@@ -3,10 +3,15 @@
 function SendDataComputer(string $key, string $name, string $data): void{
     global $bdd;
 
-    $req = $bdd->prepare('INSERT INTO status(status_key, status_name, status_data) VALUES(:key, :name :data)');
-    $req->execute(array(
-        'key' => $key,
-        'name' => $name,
-        'data' => $data
-    ));
+    try {
+        $req = $bdd->prepare('INSERT INTO status(status_key, status_name, status_data) VALUES(:key, :name, :data)');
+        $req->execute(array(
+            'key' => $key,
+            'name' => $name,
+            'data' => $data
+        ));
+    } catch (Exception $e) {
+        echo '{"statusCode" : 300, "statusMessage" : "'.$e->getMessage().'"}';
+    }
+
 }
--- server/data.php
+++ server/data.php
@@ -3,17 +3,23 @@ require_once('./app/database/bdd.php');
 require_once('./app/inc_login-register_model.php');
 require_once('./app/collectData.php');
 require_once('./app/sendData.php');
-// $computerName;
+$computerName;
 
-$url = file_get_contents('php://input');
-$data = json_decode($url, true);
-echo $data['key'];
-echo 'Coucou';
-// if(ExistKey($data['key'])){
-//     SendDataComputer($data['key'], $computerName, $data);
-// }
+$content = file_get_contents('php://input');
+$data = json_decode($content, true);
 
+
+if(ExistKey($data['key'])){
+    SendDataComputer($data['key'], $computerName, $content);
+    echo '{"statusCode" : 200, "statusMessage" : "OK"}';
+} else {
+    echo '{"statusCode" : 300, "statusMessage" : "Clé non trouvé"}';
+}
 
 // http://suricate.axdesign.fr/data.php
 // http://suricate/server/data.php
 // {"key"="hjregkbverql"}
+
+
+//statusCode
+//statusMessage 
